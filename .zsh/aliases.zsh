de5c6a450a04641c7ca9be71460f2bedd6a360d1
Adam McCrea <adam@adamlogic.com>
2008-11-15 20:32:12 -0500

    copy spicycode's zsh config and integrate my own

---
+++ .zsh/aliases.zsh
@@ -0,0 +1,16 @@
+# My aliases
+alias ss='./script/server'
+alias sc='./script/console'
+alias postgres_start='pg_ctl -D ~/.pgdata -l ~/.pgdata/psql.log start'
+alias postgres_stop='pg_ctl -D ~/.pgdata stop'
+alias mysql='/opt/local/bin/mysql5 -u root --socket=/tmp/mysql.sock'
+alias mysqladmin='/opt/local/bin/mysqladmin5 -u root --socket=/tmp/mysql.sock'
+alias mysql_config='/opt/local/bin/mysql_config5'
+alias restart='touch tmp/restart.txt'
+alias ajaxrdoc="rdoc --fmt ajax --exclude '.*generator.*' --exclude '.*test.*' --exclude '.*spec.*'"
+alias log='tail -f log/development.log'
+
+function grak {
+  mvim -p $(rak -l $@ | xargs)
+}
+
---
+++ .zsh/exports.zsh
@@ -0,0 +1,9 @@
+export PATH=/opt/local/lib/postgresql83/bin:/opt/local/bin:/opt/local/sbin:$PATH
+export RUBYOPT='rubygems'
+export GREP_OPTIONS='--color=auto' 
+export GREP_COLOR='3;33'
+export EDITOR="vim"
+export JAVA_HOME='/System/Library/Frameworks/JavaVM.framework/Home'
+export TERM=xterm-256color
+export LSCOLORS=gxfxcxdxbxegedabagacad
+export CLICOLOR=1
